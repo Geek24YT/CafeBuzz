795c1c3b4b2370bbcac1df3863c9db36354ff2eb
Geek24YT <morrikk068@gmail.com>
2023-10-21 14:43:51 +0530

    added data

---
+++ data.js
@@ -0,0 +1,44 @@
+const coffeeData = [
+    {
+      name: "Espresso",
+      description: "A strong and concentrated coffee",
+      price: 3.5,
+      photoName: "coffee/espresso.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Cappuccino",
+      description: "Espresso with steamed milk and foam",
+      price: 4.5,
+      photoName: "coffee/cappuccino.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Latte",
+      description: "Espresso with steamed milk",
+      price: 4.0,
+      photoName: "coffee/latte.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Mocha",
+      description: "Espresso with chocolate and steamed milk",
+      price: 4.7,
+      photoName: "coffee/mocha.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Cold Brew",
+      description: "Chilled coffee brewed slowly over time",
+      price: 4.5,
+      photoName: "coffee/cold_brew.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Iced Coffee",
+      description: "Chilled coffee served with ice",
+      price: 3.8,
+      photoName: "coffee/iced_coffee.jpg",
+      soldOut: true,
+    },
+  ];
